b48345eb763bfcb931bc77834060856c5e1037e6
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-05-12 14:56:37 +0300

    Edit Progress

--- js/personal.js
+++ js/personal.js
@@ -10,7 +10,6 @@ window.onscroll = function () {
     }
 };
 
-// هاي علشان لما يضغط يطلعه لفوق
 myButton.onclick = function () {
     window.scrollTo(0, 0);
 }
@@ -18,7 +17,6 @@ myButton.onclick = function () {
 /********************************************************/
 
 // Show and hide menus :-
-
 let menu = document.querySelector(".bars");
 let listMenu = document.querySelector(".parent-menu");
 
@@ -28,4 +26,17 @@ menu.onclick = function () {
     } else {
         listMenu.style.display = "block";
     }
-} 
+}
+
+/********************************************************/
+// Progrss Section
+let section = document.querySelector("#our-skills");
+let spanProgress = document.querySelectorAll("#our-skills span");
+
+window.onscroll = function () {
+    if (window.scrollY >= section.offsetTop - 300) {
+        spanProgress.forEach((span) => {
+            span.style.width = span.dataset.width;
+        });
+    }
+};
